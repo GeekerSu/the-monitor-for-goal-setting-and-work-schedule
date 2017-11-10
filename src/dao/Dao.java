2b6df22492bc4d2d30e08c7ab460140253bac657
HITHXX <1029796949@qq.com>
2017-11-10 14:51:33 +0800

    beautify login and register

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -11,7 +11,7 @@ public class Dao {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test?"
-					+ "useUnicode=true&characterEncoding=UTF-8&oldsyntax=true&zeroDateTimeBehavior=convertToNull", "root", "");
+					+ "useUnicode=true&characterEncoding=GBK&oldsyntax=true", "root", "pk188488");
 
 			System.out.println("Success Connecting to Database");
 			stat = con.createStatement();
